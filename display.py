adbe82e197f6d63f54b792d69387aa8dedfb5301
RJZheng1 <stuykid2016@yahoo.com>
2016-06-01 23:59:32 -0400

    Added code from previous directory. Sphere still bad

---
+++ display.py
@@ -0,0 +1,67 @@
+from subprocess import Popen, PIPE
+from os import remove
+
+#constants
+XRES = 500
+YRES = 500
+MAX_COLOR = 255
+RED = 0
+GREEN = 1
+BLUE = 2
+
+DEFAULT_COLOR = [0, 0, 0]
+
+def new_screen( width = XRES, height = YRES ):
+    screen = []
+    for y in range( height ):
+        row = []
+        screen.append( row )
+        for x in range( width ):
+            screen[y].append( DEFAULT_COLOR[:] )
+    return screen
+
+def plot( screen, color, x, y, z, z_buffer):
+    x = int(x)
+    y = int(y)
+    newy = YRES - 1 - y
+    if ( x >= 0 and x < XRES and newy >= 0 and newy < YRES and z > z_buffer[x][newy]):
+        screen[x][newy] = color[:]
+        z_buffer[x][newy] = z
+
+def clear_screen( screen ):
+    for y in range( len(screen) ):
+        for x in range( len(screen[y]) ):
+            screen[x][y] = DEFAULT_COLOR[:]
+
+def save_ppm( screen, fname ):
+    f = open( fname, 'w' )
+    ppm = 'P3\n' + str(len(screen[0])) +' '+ str(len(screen)) +' '+ str(MAX_COLOR) +'\n'
+    for y in range( len(screen) ):
+        row = ''
+        for x in range( len(screen[y]) ):
+            pixel = screen[x][y]
+            row+= str( pixel[ RED ] ) + ' '
+            row+= str( pixel[ GREEN ] ) + ' '
+            row+= str( pixel[ BLUE ] ) + ' '
+        ppm+= row + '\n'
+    f.write( ppm )
+    f.close()
+
+def save_extension( screen, fname ):
+    ppm_name = fname[:fname.find('.')] + '.ppm'
+    save_ppm( screen, ppm_name )
+    p = Popen( ['convert', ppm_name, fname ], stdin=PIPE, stdout = PIPE )
+    p.communicate()
+    if p.returncode != 0:
+        Popen.wait(Popen( ['imconvert', ppm_name, fname ], stdin=PIPE, stdout = PIPE ))
+        remove(ppm_name)
+    else:
+        remove(ppm_name)
+
+def display( screen ):
+    ppm_name = 'pic.ppm'
+    save_ppm( screen, ppm_name )
+    try:
+        Popen.wait(Popen( ['display', ppm_name], stdin=PIPE, stdout = PIPE ))
+    except WindowsError:
+        Popen.wait(Popen( ['imdisplay', ppm_name], stdin=PIPE, stdout = PIPE ))
